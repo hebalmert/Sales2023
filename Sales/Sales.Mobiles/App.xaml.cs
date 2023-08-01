9e85102968fdf8eaebf2c5821808829bdbf819b3
Hebalmert <merchanhebert@gmail.com>
2023-08-01 11:03:02 -0400

    Estrutura del Proyecto
    Todo esta en limpio, solo estructura principal del proyecto

---
+++ Sales/Sales.Mobiles/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Mobiles;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Sales/Sales.Mobiles/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Sales.Mobiles;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
