99d2ff3db8d312a960e0be585856982ec6114467
Hebalmert <merchanhebert@gmail.com>
2023-08-03 14:32:20 -0400

    Repositories y Consumo primer API

--- Sales/Sales.WEB/Program.cs
+++ Sales/Sales.WEB/Program.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.WEB;
+using Sales.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7150/") });
+
+//Injectamos la Interfaz y su Clase
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
 
---
+++ Sales/Sales.WEB/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Sales.WEB.Repositories;
+
+namespace Sales.WEB
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+    }
+
+}
+
